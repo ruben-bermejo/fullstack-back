49f7be8bf2ec4305f15c879b0bb402ed4732ce6b
ruben-bermejo <petergol@gmail.com>
2022-12-28 11:52:40 +0100

    Corregido test_persistencia y mejoras pylint

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
+from datetime import datetime
 from flask import Flask, request, redirect
 from persistencia import guardar_pedido_completo, Pedido
-from datetime import datetime
-import requests
 
 app = Flask(__name__)
 
 @app.route("/pizza", methods=['POST'])
 
 def procesar_pedido():
-    """Crea un fichero con el nombre y apellidos recibidos desde frontend y redirige a solicita_pedido"""
+    """Crea fichero con nombre y apellidos desde frontend y redirige a solicita_pedido"""
     nombre = request.form.get("nombre")
     apellidos = request.form.get("apellidos")
     telefono = request.form.get("telefono")
@@ -19,29 +18,26 @@ def procesar_pedido():
     peppe = request.form.get("peppe")
     champis = request.form.get("champis")
     aceitunas = request.form.get("aceitunas")
-
     pedido = Pedido(nombre, apellidos)
-    if(telefono):
+    if telefono:
         pedido.telefono = telefono
-    if(fecha_nacimiento):
+    if fecha_nacimiento:
         pedido.nacimiento = datetime.strptime(fecha_nacimiento, '%Y-%m-%d').strftime('%d/%m/%Y')
-    if(direccion):
+    if direccion:
         pedido.direccion = direccion
-    if(tamano_pizza):
+    if tamano_pizza:
         pedido.tamano = tamanos_pizza.get(tamano_pizza)
-    if(york):
+    if york:
         pedido.ingredientes.append(ingredientes.get(york))    
-    if(peppe):
+    if peppe:
         pedido.ingredientes.append(ingredientes.get(peppe))    
-    if(champis):
+    if champis:
         pedido.ingredientes.append(ingredientes.get(champis))    
-    if(aceitunas):
+    if aceitunas:
         pedido.ingredientes.append(ingredientes.get(aceitunas))    
-    
     print(pedido)
     crear_fichero()
     guardar_pedido_completo(pedido)
-
     return redirect("http://localhost/pizza-fullstack/solicita_pedido.html", code = 302)
     
 def crear_fichero():
--- test_persistencia.py
+++ test_persistencia.py
@@ -1,73 +1,20 @@
-from io import StringIO
+from persistencia import guardar_pedido
 
-def guardar_pedido(nombre, apellidos):
-    """ Escribe una línea con el formato "nombre apellidos " en el fichero "pedidos.txt" 
-    
-        Atributos:
-            nombre: El nombre de la persona que hace el pedido
-            apellidos: Los apellidos de la persona que hace el pedido
-    """
-    with open("pedidos.txt", "a", encoding="utf-8") as file:
-        file.write(nombre + " " + apellidos + "\n")
+# Lista de pedidos con dos atributos: Nombre y Apellidos 
+pedidos = [ {"nombre": "José Rubén", "apellidos": "Bermejo Sanz"},
+            {"nombre": "Michael", "apellidos": "Scott"}]
+
+def crear_fichero():
+    """Crea un fichero "pedidos.txt" vacío."""
+    with open("pedidos.txt", "w", encoding="utf-8") as file:
+        file.write("")
         file.close()
 
-def guardar_pedido_completo(pedido: object):
-    """ Escribe una línea con el formato "nombre|apellidos|...|ingredientes " en el fichero "pedidos.txt" 
-    
-        Atributos:
-            pedido: Objeto de la clase Pedido que contiene todos los datos del pedido
-    """
-    field_separator = "|"
-    sb = StringBuilder()
-    sb.append(pedido.nombre)
-    sb.append(field_separator)
-    sb.append(pedido.apellidos)
-    sb.append(field_separator)
-    sb.append(pedido.telefono)
-    sb.append(field_separator)
-    sb.append(pedido.nacimiento)
-    sb.append(field_separator)
-    sb.append(pedido.direccion)
-    sb.append(field_separator)
-    sb.append(pedido.tamano)
-    sb.append(field_separator)
-    sb.append(str(pedido.ingredientes))
-    
-    with open("pedidos.txt", "a", encoding="utf-8") as file:
-        file.write(str(sb))
-        file.close()
+def escribir_fichero():
+    """Invoca a la función `persistencia.guardar_pedido()` por cada elemento 
+    en la lista de pedidos."""
+    for person in pedidos:
+        guardar_pedido(person["nombre"], person["apellidos"])
 
-class StringBuilder:
-    _file_str = None
-
-    def __init__(self):
-        self._file_str = StringIO()
-
-    def append(self, str):
-        self._file_str.write(str)
-
-    def __str__(self):
-        return self._file_str.getvalue()
-
-class Pedido:
-    """Esta clae define los datos de un pedido"""
-
-    def __init__(self, nombre, apellidos):
-        self.nombre = nombre
-        self.apellidos = apellidos
-        self.telefono = None;
-        self.nacimiento = None;
-        self.direccion = None;
-        self.tamano = None;
-        self.ingredientes = [];
-
-    def __str__(self):
-        detalles = ''
-        detalles += f'Nombre               : {self.nombre}\n'
-        detalles += f'Apellidos            : {self.apellidos}\n'
-        detalles += f'Teléfono             : {self.telefono}\n'
-        detalles += f'Fecha nacimiento     : {self.nacimiento}\n'
-        detalles += f'Dirección            : {self.direccion}\n'
-        detalles += f'Tamaño pizza         : {self.tamano}\n'
-        detalles += f'Ingredientes         : {self.ingredientes}\n'
-        return detalles
+crear_fichero()
+escribir_fichero()
